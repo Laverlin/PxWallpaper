b3c6308b91fb2d845fc21c924ae9862708410ce5
Laverlin <ilaverlin@gmail.com>
2017-02-09 00:25:37 +0300

    Use config for parameters

--- PxWallpaper/PxWallpaper.py
+++ PxWallpaper/PxWallpaper.py
@@ -1,17 +1,22 @@
 import requests
 import json
 import shutil
+import os
+
 
 def main():
     print("let's start")
 
-    jsonResult = requests.get("https://api.500px.com/v1/photos?feature=popular&image_size=2048&rpp=1&consumer_key=" + getConsumerKey()).json()
+    consumer_key, image_path, image_file = GetConfig()
+    requestUrl = "https://api.500px.com/v1/photos?feature=popular&image_size=2048&rpp=1&consumer_key={0}".format(consumer_key)
+    jsonResult = requests.get(requestUrl).json()
     imageUrl = jsonResult['photos'][0]['images'][0]['url']
     imageFormat = jsonResult["photos"][0]["images"][0]["format"]
 
+    photoFullName = os.path.join(image_path, image_file)
     imageResponse = requests.get(imageUrl, stream = True)
     if imageResponse.status_code == 200:
-        with open("todaysBest.jpg", "wb") as imageFile:
+        with open(photoFullName, "wb") as imageFile:
             imageResponse.raw.decode_content = True
             shutil.copyfileobj(imageResponse.raw, imageFile)
 
@@ -28,11 +33,11 @@ def main():
 
 
 
-def getConsumerKey():
+def GetConfig():
     
     with open('config.json') as config:
-        consumerKey = json.loads(config.read())
-        return consumerKey["authentication"]["consumer_key"]
+        config = json.loads(config.read())
+        return config["authentication"]["consumer_key"], config["image_path"], config["image_file"]
 
 if __name__ == "__main__":
     main()
